ca68bad2a6e16ec8f84b580dbcb476342d5c2e28
Jys_ <jiayu.sun@iispadovano.it>
2026-03-09 13:43:24 +0100

    Add files via upload

---
+++ frontend/home.php
@@ -0,0 +1,38 @@
+
+
+<html>
+    <head> 
+        <title>F1 Race</title>
+        <link rel="stylesheet" href="css/home.css">
+    </head>
+    <body>
+        <div id="inner_body">
+
+            <div id="navbar">
+                <div id="navbar_title">
+                    <h1>F1 Race</h1>
+                </div>
+                <div id="navbar_right">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </div>
+            </div>
+
+            <div id="navigation_section">
+                <div id="links">
+                    <a href="#">Piste</a>
+                    <a href="#">Championship</a>
+                    <a href="#">Leaderboard</a>
+                    <a href="#">Prenota Championship</a>
+                </div>
+
+                <div id="leclerc">
+                    <img src="img/Leclerc.jpg" alt="Charles Leclerc">
+                </div>
+            </div>
+
+        </div>
+
+        
+    </body>
+</html>
